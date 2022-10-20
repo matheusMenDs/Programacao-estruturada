b81d2454280a15784158a3400e68cd98e5677703
matheusMenDs <matheusmendesoliveira10@gmail.com>
2022-10-20 12:01:47 -0300

    Lista Vetor

---
+++ Lista Vetor/Lista.6.cpp.cpp
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>                //falta corrigir, está lendo apenas 3 funcionarios
+                                  //deveria perguntar primeiro, quantos funcionarios
+int main(){                      //a pessoa vai querer ler (primeira coisa do codigo)
+    char matricula[4][4];
+    float salario[4];
+	float menorValor = 999999;
+	float maiorValor = 0;
+
+	for (int i = 1; i < 4; i++){
+    printf("Digite o Numero da Matricula do %d Empregado: ", i);
+    scanf("%s", &matricula[i]);
+    printf("Digite o Salario do %d Empregado: ", i);
+    scanf("%f", &salario[i]);
+	}
+
+	system("clear||cls");
+
+	for (int i = 1; i < 4; i++){
+    printf("%s recebe %.2f Reais de Salario\n", matricula[i], salario[i]);
+    for (int i = 1; i < 4; i++){
+        if (salario[i] < menorValor){
+            menorValor = salario[i];}
+ 		} 
+	}
+	
+	printf("Menor Salario: %.2f\n", menorValor);
+	
+	for (int i = 0; i < 4; i++){
+    if (salario[i] > maiorValor){
+        maiorValor = salario[i];}
+	}
+
+	printf("Maior Salario: %.2f\n", maiorValor);
+
+return 0;
+}
+
---
+++ Lista Vetor/Lista.7.cpp.cpp
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>     
+
+int main() {                                         //corrigir para colocar ATÉ 100 CD'S
+	double cd[5], cd10[5], cdx[5], porcentagem;    //está lendo apenas 5
+	int i;
+	 
+	for(i=0;i<5;i++) {
+	 	printf("Digite o valor do CD: ");
+	 	scanf("%lf", &cd[i]);
+	}
+	 
+	for(i=0;i<5;i++){
+		cd10[i] = cd[i] + cd[i] * 0.10;
+	}
+	for(i=0;i<5;i++) {
+		printf("Esse e o valor com juros:\n %lf\n", cd10[i]);
+
+	}
+	
+	printf("Vendedor, digite a procentagem que voce deseja acrescentar: ");
+	scanf("%lf", &porcentagem);
+
+	for(i=0;i<5;i++){
+		cdx[i] = cd[i] + cd[i] * (porcentagem/100);
+	}	
+	
+	for(i=0;i<5;i++) {
+		printf("\nEsse e o valor com acrescimo de %lf:\n %lf", porcentagem, cdx[i]);
+	}
+	 
+return 0;
+}
