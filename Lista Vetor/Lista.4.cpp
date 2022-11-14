43699612fd7c48dd053692e19906d2943b3726f6
MatheusMendes <111511128+matheusMenDs@users.noreply.github.com>
2022-11-13 21:29:44 -0300

    Quest 4

--- Lista Vetor/Lista.4.cpp
+++ Lista Vetor/Lista.4.cpp
@@ -1,17 +1,17 @@
 #include <stdio.h>
 
-int main(){                   //esta lendo 30 letras, precisa concertar para que pergunte
-	char letras[30];         //quantas letras a pessoa quer ler, pra nao ser sempre 30...
+int main(){                   
+	char letras[10];        
 	char i;
 	
-	for(i=0; i<30; i++){
+	for(i=0; i<10; i++){
 		printf("Digite a Letra: ");
 		scanf("%s", &letras[i]);
 	}
 	
 	printf("\nORDEM INVERSA: ");
 	
-	for(i=29; i>=0; i--){
+	for(i=9; i>=0; i--){
 		printf("%c ", letras[i]);
 	}
 
