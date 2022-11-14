6c272e1fbe0973a016b23dad54a6523d33292187
MatheusMendes <111511128+matheusMenDs@users.noreply.github.com>
2022-11-13 23:27:07 -0300

    Add files via upload

---
+++ Lista Vetor e Matriz/Lista9.cpp
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(){
+    int matriz[10][3];
+    int piorNota[3];
+	int pior = 100;
+	int aluno[3];
+	int materia[3];
+	int i;
+	int j;
+    
+	for(i=0; i<10; i++){
+        for (j=0; j<3; j++){
+            printf("Informe a nota %d do aluno: %d [%d],[%d]: ", j, i, i, j);
+            scanf("%d", &matriz[i][j]);
+        } 
+    }
+    
+    for (j=0; j<3; j++){
+        pior = 100;
+        for(i=0; i<10; i++){
+            if (matriz[i][j] < pior){
+                pior = matriz[i][j];
+                piorNota[j] = matriz[i][j];
+                aluno[j] = i;
+                materia[j] = j;
+            }
+        } 
+    }
+    
+    for (i=0; i<3; i++){
+        printf("Pior aluno na materia: %d\nFoi: %d\nCom %d", i, aluno[i], piorNota[i]);
+    }
+    
+}
