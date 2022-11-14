e52f6fa02134280827b002384be0454c9d7bc556
MatheusMendes <111511128+matheusMenDs@users.noreply.github.com>
2022-11-13 23:00:11 -0300

    Quest 5

--- Vetor e Matriz/Lista5.cpp
+++ Vetor e Matriz/Lista5.cpp
@@ -35,7 +35,7 @@ int main(){
 
     for(i=0; i<4; i++){
         for(j=0; j<4; j++){
-         printf("C(%d,%d) = %d\n", i, j, c[i][j]);
+            printf("C(%d,%d) = %d\n", i, j, c[i][j]);
         }
     }
 
