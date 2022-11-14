be5a3aa03f7a2c0f58d704609916ba4605b5773f
MatheusMendes <111511128+matheusMenDs@users.noreply.github.com>
2022-11-13 22:56:53 -0300

    Quest 2

--- Vetor e Matriz/Lista2.cpp
+++ Vetor e Matriz/Lista2.cpp
@@ -4,7 +4,7 @@
 int main(){
     int m[10],m1[10],m2[10],i,calc,par[10],impar[10];
     
-	for ( i = 0; i < 10; i++){
+	for( i = 0; i < 10; i++){
         printf("Informe o numero %d: ",i);
         scanf("%d",&m[i]);
         calc = m[i] %2;
@@ -18,13 +18,13 @@ int main(){
         }  
     }
     
-	for ( i = 0; i < 10; i++){
-      if (impar[i] ==1){
+	for( i = 0; i < 10; i++){
+      		if (impar[i] ==1){
        printf("Numero impar eh: %d na posicao: %d\n", m2[i],i);
       }  
     }
    
-    for ( i = 0; i < 10; i++){
+    for( i = 0; i < 10; i++){
         if (par[i] ==1 ){
         printf("Numero par eh: %d na posicao: %d\n", m1[i],i);
     	}
